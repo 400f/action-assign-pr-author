c4c5ae9745c785b4c3a1948c343937fc337b8145
resessh <info@resessh.net>
2021-03-06 18:28:13 +0900

    initial

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,40 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+
+const ctx = github.context
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    // const token = core.getInput('GITHUB_TOKEN', {required: true})
+    // const author = ctx.payload
+    core.debug(JSON.stringify(ctx.payload.pull_request))
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // const octokit = github.getOctokit(token)
 
-    core.setOutput('time', new Date().toTimeString())
+    // const {owner, repo} = ctx.repo
+    // const pull_number = ctx.payload.pull_request?.number
+
+    // if (!pull_number) {
+    //   core.debug('There is no pull_number')
+    //   return
+    // }
+    // const {data: pullRequest} = await octokit.pulls.get({
+    //   owner,
+    //   repo,
+    //   pull_number
+    // })
+
+    // if (pullRequest.assignee) {
+    //   core.debug('This PR already has been assigned')
+    //   return
+    // }
+
+    // await octokit.issues.addAssignees({
+    //   owner,
+    //   repo,
+    //   issue_number: pull_number,
+    //   assignees:
+    // })
   } catch (error) {
     core.setFailed(error.message)
   }
